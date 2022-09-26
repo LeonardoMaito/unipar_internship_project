f726b668b460ebb68db82d5f8ce751f561a92bbb
Leonardo Torri Maito <80430708+LeonardoMaito@users.noreply.github.com>
2022-09-26 16:08:45 -0300

    ID Generator

--- app/src/main/java/com/leonardomaito/autocommobile/activities/NewOsThirdActivity.java
+++ app/src/main/java/com/leonardomaito/autocommobile/activities/NewOsThirdActivity.java
@@ -2,6 +2,7 @@ package com.leonardomaito.autocommobile.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,11 @@ import android.widget.EditText;
 import com.leonardomaito.autocommobile.controllers.ServiceOrderController;
 import com.leonardomaito.autocommobile.models.Client;
 import com.leonardomaito.autocommobile.models.Vehicle;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
 
 import autocommobile.R;
 
@@ -21,6 +27,7 @@ public class NewOsThirdActivity extends AppCompatActivity {
     private EditText etPaymentForm;
     private EditText etDate;
     private EditText etValue;
+    private Date currentTime = Calendar.getInstance().getTime();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +40,15 @@ public class NewOsThirdActivity extends AppCompatActivity {
         Client newClient = data.getParcelable("novoCliente");
         Vehicle newVehicle =  data.getParcelable("novoCarro");
 
+        Date currentTime = Calendar.getInstance().getTime();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
         returnMenuOs = findViewById(R.id.btReturnMenuOs);
         etService = findViewById(R.id.etServiceInput);
         etObservation = findViewById(R.id.etObservationInput);
         etPaymentForm = findViewById(R.id.etPaymentFormInput);
         etDate = findViewById(R.id.etDateInput);
-        etDate.setText("0");
+        etDate.setText(formatter.format(currentTime));
         etValue = findViewById(R.id.etValue);
 
         returnMenuOs.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +58,8 @@ public class NewOsThirdActivity extends AppCompatActivity {
                 serviceOrderController.returnNewServiceOrder(etService, etObservation, etPaymentForm, newClient,
                         newVehicle, etDate, etValue);
 
+                Intent intent = new Intent(getApplicationContext(), OsActivity.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/leonardomaito/autocommobile/controllers/ServiceOrderController.java
+++ app/src/main/java/com/leonardomaito/autocommobile/controllers/ServiceOrderController.java
@@ -5,10 +5,13 @@ import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
@@ -30,6 +33,7 @@ public class ServiceOrderController {
     private String osObservation;
     private String osPaymentForm;
     private double osValue;
+    private long idValue;
     Map<String, Object> data = new HashMap<>();
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference docRef =
@@ -37,20 +41,22 @@ public class ServiceOrderController {
                     .document("clienteTeste")
                     .collection("ServiceOrder");
 
+    private DocumentReference idRef =
+            db.collection("cliente")
+                    .document("clienteTeste")
+                    .collection("ServiceOrder")
+                    .document("reservedId");
+
     public void returnNewServiceOrder(EditText etService, EditText etObservation, EditText etPaymentForm
     , Client newClient, Vehicle newVehicle, EditText etDate, EditText etValue){
-
-        Date currentTime = Calendar.getInstance().getTime();
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 
         osService = etService.getText().toString();
         osObservation = etObservation.getText().toString();
         osPaymentForm = etPaymentForm.getText().toString();
-        etDate.setText(formatter.format(currentTime));
-        osValue=  etValue.getText().toString().isEmpty() ? 0: Double.parseDouble(etValue.getText().toString());
+        osValue = Double.parseDouble(etValue.getText().toString());
 
         ServiceOrder newServiceOrder = new ServiceOrder.ServiceOrderBuilder(newClient,
-                newVehicle,osService,osPaymentForm, 1, osValue, etDate.getText().toString())
+                newVehicle,osService,osPaymentForm, 0, osValue, String.valueOf(etDate.getText()))
                 .observation(osObservation)
                 .build();
 
@@ -59,8 +65,27 @@ public class ServiceOrderController {
 
     public void sendDataToFirestore(ServiceOrder serviceOrder){
 
-        data.put("serviceOrder", serviceOrder);
-        docRef.add(data);
 
+
+        idRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    idValue =  (long) document.get("id");
+                    Log.e("Result","ID" + idValue);
+
+                }
+            }
+        });
+
+        data.put("serviceOrder", serviceOrder);
+        docRef.add(data).addOnSuccessListener(documentReference -> {
+            String id = documentReference.getId();
+            docRef.document(id).update("serviceOrder.id", FieldValue.increment(idValue));
+            idRef.update("id", FieldValue.increment(1));
+            Log.e("Result","Funcionou");
+
+        });
     }
 }
